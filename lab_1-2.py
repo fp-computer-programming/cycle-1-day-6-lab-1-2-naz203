fdbdafc511666f8a7525040db7c2a6b5be00b14b
naz <p24nthompson@fairfieldprep.org>
2023-09-14 10:58:34 -0400

    submit cycle day 1 labs 6

--- lab_1-2.py
+++ lab_1-2.py
@@ -3,15 +3,16 @@
 # 2) Add the heading to the first line of your file.
 
 # 3) Create a variable and set it equal to your first name. Make sure the variable follows all the variable naming rules.
-
-# 4) Create a print statement to display your variable when your code is run.
-
+n = "nazeer"
+# 4) Create a print statement to display your variable when your code is run 
+print (n)
 # 5) Run the program.
 
 # 6) Hit enter twice. Then hit Tab once or Space four times.
 
 # 7) Create a variable and set it equal to "your favorite hobby". Make sure the variable follows all the variable naming rules.
-
+    a = "football"
 # 8) Create a print statement to display your variable when your code is run.
-
+print (a)
 # 9) What happens when you run the program again?
+# that i had got an error 
